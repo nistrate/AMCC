f764adf5ecc196be4f16e2b6bc18912966f37104
Nicolae Istrate <istratenicu@gmail.com>
2022-03-07 16:42:40 -0800

    added a project report and commented some of the main code.

--- Logic/Logic.ino
+++ Logic/Logic.ino
@@ -22,15 +22,15 @@ myDCMotorLibrary car(LeftBackIO, LeftBackA, LeftBackB, RightBackIO, RightBackA, 
 int rightDistance = 0, leftDistance = 0, middleDistance =0;
 
 int posLow = 8;    // variable to store the servo position
-int servoPosition;
-int newServoPosition;
+int servoPosition; // keeps track of the servo position
+int newServoPosition; // keeps track of the new servo position
 Servo myservo;  // create servo object to control a servo
 #define ServoPin 9  // servo pin
 
 
 //SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN); // create distance sensor object
 long a;         // variable to store the distance in cm
-int speed = 100;
+int speed = 100; // controls speed of back motors
 int thresholdDistance = 35; //cm
 
 ///////////////////////////////
@@ -63,7 +63,7 @@ void moveFromToAngle(int pos0, int pos1)
 //////////////////////
 //// Ultrasonic Function
 
-int Distance_test()
+int Distance_test() // measures distance and transforms to cm
 {
   digitalWrite(Trig, LOW);
   delayMicroseconds(2);
@@ -81,26 +81,28 @@ void setup() {
 
   //Servo
   myservo.attach(ServoPin);  // attaches the servo on pin 9 to the servo object
-  myservo.write(posLow);
+  myservo.write(posLow);     // moves servo to position Low
 
    Serial.begin(9600); // spiecfies the output channel
    delay(500);
 
   // Ultrasound HC04
-  pinMode(Echo, INPUT);
-  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);    // connects distance sensor to arduino pin
+  pinMode(Trig, OUTPUT);   // connects distance sensor to arduino pin
 
-  car.PowerFront(HIGH);
-  car.PowerBack(HIGH,HIGH);
+  car.PowerFront(HIGH);    // supply power to front motor
+  car.PowerBack(HIGH,HIGH); // supply power to back motors
 }
 
 
 void loop() {
  
+   // move servo to 90 degrees positin (sensors pointing forwards)
    servoPosition = 90;
    myservo.write(servoPosition);
    delay(500);
-
+   
+   // Measure and display distance
    middleDistance = Distance_test();
    Serial.print("Measurement at angle: ");
    Serial.print(servoPosition);
@@ -108,7 +110,10 @@ void loop() {
    Serial.print(middleDistance);
    Serial.println(" cm");
 
-
+   // If distance < threshold move forward
+   // once reach the threshold, move servo to left right measure distance
+   // chose the largest distace and turn in that direction
+   // iff all distances <= threshold move backwards
    if (middleDistance <= thresholdDistance){
     car.PowerOff();
     delay(500);
@@ -167,16 +172,5 @@ void loop() {
     car.PowerBack(HIGH,HIGH);
     car.forward(speed);
    }
-
-    // car.PowerBack(100,100);
-    // delay(100);
-    // car.forward(speed);
-    // delay(1000);
-    // car.backward(1000, speed);
-    // car.PowerOff();
-    // delay(1000);
-    // leftForwardTurn(1000);
-    // delay(1000);
-    // leftForwardTurn(1000);
     
 }
